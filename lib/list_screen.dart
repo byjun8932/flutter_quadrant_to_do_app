2b96ca77c3c556466e680524a7ea2a4048ae89d7
sangjun <byjun8932@gmail.com>
2024-05-08 17:56:51 +0900

    refactor: make Todo model class and present to ListScreen

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quadrant_to_do_app/create_screen.dart';
+import 'package:flutter_quadrant_to_do_app/todo.dart';
 
-class ListScreen extends StatelessWidget {
+class ListScreen extends StatefulWidget {
   const ListScreen({super.key});
+
+  @override
+  State<ListScreen> createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  final todos = [
+    Todo(
+      title: 'title 1',
+      dateTime: 12312312,
+    ),
+    Todo(
+      title: 'title 2',
+      dateTime: 12312312,
+    ),
+    Todo(
+      title: 'title 3',
+      dateTime: 99999999,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -12,16 +33,10 @@ class ListScreen extends StatelessWidget {
         title: const Text('Todo 리스트'),
       ),
       body: ListView(
-        children: const [
-          ListTile(
-            title: Text('title 1'),
-            subtitle: Text('subtitle 1'),
-          ),
-          ListTile(
-            title: Text('title 2'),
-            subtitle: Text('subtitle 2'),
-          ),
-        ],
+        children: todos.map((todo) => ListTile(
+          title: Text(todo.title),
+          subtitle: Text('${todo.dateTime}'),
+        )).toList(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
